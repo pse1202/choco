27f785513f0efbc8871a64a02a7e51d87074a73b
ssut <ssut@ssut.me>
2014-05-19 20:11:16 +0900

    Add reload, exit function in CLI

--- choco/cli.py
+++ choco/cli.py
@@ -2,8 +2,18 @@
 """
 CHOCO CLI(Command Line Interface) Handler
 """
+import sys
+import os
 import shlex
 import inspect
+import imp
+import traceback
+try:
+    import readline
+except:
+    print >> sys.stderr, 'GNU Readline is not supported by this environment'
+from datetime import datetime
+from modules import modules
 
 class ChocoCLI(object):
     def __init__(self, choco):
@@ -22,9 +32,34 @@ class ChocoCLI(object):
                     c = cmd[0]
                     if c in self.commands:
                         del cmd[0]
-                        print self.commands[c](*cmd)
-            except:
-                pass
+                        self.commands[c](*cmd)
+            except Exception, e:
+                traceback.print_exc()
 
-    def test(self):
-        return 1
+    def status(self):
+        now = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+        recv_count = self.choco.cache.get('choco:count:recv')
+        exec_count = self.choco.cache.get('choco:count:exec')
+        sent_count = self.choco.cache.get('choco:count:sent')
+        room_count = self.choco.cache.hlen('choco:rooms')
+        session_count = self.choco.cache.hlen('choco:sessions')
+
+        print '[C] {0}'.format(now)
+        print 'RECV     : %s' % recv_count
+        print 'EXEC     : %s' % exec_count
+        print 'SENT     : %s' % sent_count
+        print 'ROOMS    : %s' % room_count
+        print 'SESSIONS : %s' % session_count
+        print 'BUSY     : %s/%s' % (self.choco.working_count.value, self.choco.config.THREAD_COUNT)
+
+    def reload(self):
+        print 'RELOADING MODULES ..'
+        self.choco.module.rules = []
+        self.choco.module.functions = {}
+        for m in modules:
+            imp.reload(m)
+        print 'RELOAD COMPLETE !'
+
+    def exit(self):
+        self.choco.exit.value = True
+        os._exit(1)
