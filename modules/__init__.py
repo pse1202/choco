c0df04faf3b4061bb019f182b19557df36e4d615
ssut <ssut@ssut.me>
2014-05-19 20:09:56 +0900

    Add module list for reload modules

--- modules/__init__.py
+++ modules/__init__.py
@@ -9,6 +9,7 @@ from collections import namedtuple
 Result = namedtuple('Result', ['type', 'content'])
 choco = None
 module = None
+modules = []
 
 def init_module(cc, ep):
     global choco, module
@@ -24,7 +25,7 @@ def module_loader(home, config):
         if name in config.EXCLUDED_MODULES: continue
         if fn.endswith('.py') and not fn.startswith('_'):
             fn = os.path.join(os.path.dirname(os.path.realpath(__file__)), fn)
-            try: imp.load_source(name, fn)
+            try: modules.append(imp.load_source(name, fn))
             except Exception, e:
                 traceback.print_exc()
                 print >> sys.stderr, "Error loading %s: %s" % (name, e)
