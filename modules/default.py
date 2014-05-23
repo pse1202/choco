9de55399137b49c39d00c47c83c42ce699a2720d
ssut <ssut@ssut.me>
2014-05-23 10:38:39 +0900

    Fix: line length over 80 characters

--- modules/default.py
+++ modules/default.py
@@ -13,11 +13,14 @@ def leave(request):
     if not leave_list.exists(request.session.id):
         leave_list.append(request.session.id)
         leave_count += 1
-    if (session_count <= 3 and leave_count >= session_count - 1) or (session_count > 3 and leave_count >= 3):
+    if (session_count <= 3 and leave_count >= session_count - 1) \
+        or (session_count > 3 and leave_count >= 3):
         return KakaoResponse(None, content_type=ContentType.Leave)
     else:
         session_count = str(session_count)
-        s = u"%s명 중 %s명이 나가를 외치셨습니다.\r\n방 인원이 3명 이상일 경우 최소 3명이 나가를 외치셔야 합니다!\r\n(그 이하일 경우 모두가 나가를 외치셔야 합니다)" % (session_count, leave_count)
+        s = u"%s명 중 %s명이 나가를 외치셨습니다."  % (session_count, leave_count) + \
+            u"\r\n방 인원이 3명 이상일 경우 최소 3명이 나가를 외치셔야 합니다!" + \
+            u"\r\n(그 이하일 경우 모두가 나가를 외치셔야 합니다)"
         return KakaoResponse(s)
 
 @module.route(ur'([가-힣ㄱ-ㅎ\d\s]+)\s{0,}?해봐', re=True)
